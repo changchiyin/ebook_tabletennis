cd6e67816a207ad6a481222ad10a8e128436dade
andy <andy870622@gmail.com>
2021-11-05 23:41:48 +0800

    Initial Commit

---
+++ ebook_tabletennis/ebook_tabletennisApp.swift
@@ -0,0 +1,17 @@
+//
+//  ebook_tabletennisApp.swift
+//  ebook_tabletennis
+//
+//  Created by User15 on 2021/11/5.
+//
+
+import SwiftUI
+
+@main
+struct ebook_tabletennisApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
